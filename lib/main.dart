d1f437630d942105d0c9152fa9fd0760abf8c7a9
Rizwan Ali <rizwanali96960@gmail.com>
2025-01-07 12:34:42 +0500

    done

---
+++ lib/main.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: (context, _) {
+        return MaterialApp(
+          theme: ThemeData(
+            useMaterial3: false,
+          ),
+          debugShowCheckedModeBanner: false,
+          home: const PaintDemoApp(),
+        );
+      },
+    );
+  }
+}
+
+class PaintDemoApp extends StatefulWidget {
+  const PaintDemoApp({super.key});
+
+  @override
+  State<PaintDemoApp> createState() => _PaintDemoAppState();
+}
+
+class _PaintDemoAppState extends State<PaintDemoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: AnimatedTextBody(),
+      ),
+    );
+  }
+}
+
+class AnimatedTextBody extends StatefulWidget {
+  const AnimatedTextBody({super.key});
+
+  @override
+  State<AnimatedTextBody> createState() => _AnimatedTextBodyState();
+}
+
+class _AnimatedTextBodyState extends State<AnimatedTextBody> {
+  late String text;
+  int? selectedIndex;
+  int? right;
+  int? left;
+
+  final List<GlobalKey> _textKeys = [];
+
+  @override
+  void initState() {
+    super.initState();
+    text = 'Animated Text';
+    _textKeys.addAll(List.generate(text.length, (_) => GlobalKey()));
+  }
+
+  void _updateSelectedIndexFromPosition(Offset position) {
+    for (int i = 0; i < _textKeys.length; i++) {
+      final key = _textKeys[i];
+      final renderBox = key.currentContext?.findRenderObject() as RenderBox?;
+      if (renderBox == null) return;
+      final boxPosition = renderBox.localToGlobal(Offset.zero);
+      final boxSize = renderBox.size;
+      if (position.dx >= boxPosition.dx &&
+          position.dx <= boxPosition.dx + boxSize.width) {
+        setState(() {
+          selectedIndex = i;
+          left = i - 1;
+          right = i + 1;
+        });
+        break;
+      }
+    }
+  }
+
+  double _getFontSize(int index) {
+    if (selectedIndex == index) {
+      return 50;
+    } else if (left == index || right == index) {
+      return 40;
+    } else {
+      return 30;
+    }
+  }
+
+  Color _getTextColor(int index) {
+    if (selectedIndex == index) {
+      return Colors.deepOrange;
+    } else if (left == index || right == index) {
+      return Colors.deepOrange.withOpacity(0.8);
+    } else {
+      return Colors.white;
+    }
+  }
+
+  void _resetSelection() {
+    setState(() {
+      selectedIndex = null;
+      left = null;
+      right = null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final data = text.split('');
+    return GestureDetector(
+      onHorizontalDragUpdate: (details) {
+        _updateSelectedIndexFromPosition(details.globalPosition);
+      },
+      onHorizontalDragEnd: (_) {
+        _resetSelection();
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: List.generate(data.length, (index) {
+          return TweenAnimationBuilder(
+            key: _textKeys[index],
+            tween: Tween<double>(
+              begin: 30,
+              end: _getFontSize(index),
+            ),
+            duration: const Duration(milliseconds: 100),
+            builder: (context, size, child) {
+              return Text(
+                data[index],
+                style: TextStyle(
+                  color: _getTextColor(index),
+                  fontWeight: FontWeight.w800,
+                  fontSize: size,
+                ),
+              );
+            },
+          );
+        }),
+      ),
+    );
+  }
+}
